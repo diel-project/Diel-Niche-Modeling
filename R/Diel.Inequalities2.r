3b16ef0ef2b7096727c1f5e12ca1734aa894965b
bgerber123 <bgerber@uri.edu>
2022-06-06 11:33:52 -0400

    Update Diel.Inequalities2.r
    0.01 constraint on zero to 0.001

--- R/Diel.Inequalities2.r
+++ R/Diel.Inequalities2.r
@@ -3,25 +3,20 @@
 #' Multinomial model inequalities for diel hypotheses
 #' @param e Default is 0.05. A single value of variation for probabilities. If specified, it will be applied to all hypotheses, regardless of whether individual epsilon hypotheses values are specified. 
 #' @param e.D Default is 0.05. A single value of variation for the Diurnal hypothesis.
-#' @param e.Dc Default is 0.05. A single value of variation for the Diurnal-cathemeral hypothesis.
 #' @param e.Dcr Default is 0.05. A single value of variation for the Diurnal-crepuscular hypothesis.
 #' @param e.N Default is 0.05. A single value of variation for the Nocturnal hypothesis.
 #' @param e.Nd Default is 0.05. A single value of variation for the Nocturnal-diurnal hypothesis.
-#' @param e.Nc Default is 0.05. A single value of variation for the Nocturnal-cathenerak hypothesis.
 #' @param e.Ncr Default is 0.05. A single value of variation for the Nocturnal-crepuscular hypothesis.
 #' @param e.CR Default is 0.05. A single value of variation for the Crepuscular hypothesis.
 #' @param e.CRd Default is 0.05. A single value of variation for the Crepuscular-diurnal hypothesis.
 #' @param e.CRn Default is 0.05. A single value of variation for the Crepuscular-nocturnal hypothesis.
-#' @param e.CRc Default is 0.05. A single value of variation for the Crepuscular-cathemeral hypothesis.
 #' @param e.EC  Default is 0.05. A single value of variation for the Evan Cathemeral hypothesis.
 #' @param e.AC   Default is 0.10. A single value of variation for the Available Cathemeral hypothesis.
 #' @param xi.D Default c(0.90,0.95). A vector of the lower threshold value and the most likely value, respectively for the Diurnal hypothesis.
 #' @param xi.Dn Default c(0.80,0.20,0.90,0.10). A vector of the lower threshold value for the primary probability, upper threshold value of the secondary probability, most likely primary probability, and most likely secondary probability value, respectively for the Diurnal-nocturnal hypothesis.
-#' @param xi.Dc Default c(0.80,0.01,0.90,0.10). A vector of the lower threshold value for the primary probability, upper threshold value of the secondary probability, most likely primary probability, and most likely secondary probability value, respectively for the Diurnal-cathemeral hypothesis.
 #' @param xi.Dcr Default c(0.80,0.2,0.90,0.10). A vector of the lower threshold value for the primary probability, upper threshold value of the secondary probability, most likely primary probability, and most likely secondary probability value, respectively for the Diurnal-crepuscular hypothesis.
 #' @param xi.N  Default c(0.80,0.90). A vector of the lower threshold value and most likely value,, respectively for the Nocturnal hypothesis.
 #' @param xi.Nd  Default c(0.80,0.2,0.90,0.10). A vector of the lower threshold value for the primary probability, upper threshold value of the secondary probability, most likely primary probability, and most likely secondary probability value, respectively for the Nocturnal-diurnal hypothesis.
-#' @param xi.Nc   Default c(0.80,0.01,0.90,0.10). A vector of the lower threshold value for the primary probability, upper threshold value of the secondary probability, most likely primary probability, and most likely secondary probability value, respectively for the Nocturnal-cathemeral hypothesis.
 #' @param xi.Ncr  Default c(0.80,0.2,0.90,0.10). A vector of the lower threshold value for the primary probability, upper threshold value of the secondary probability, most likely primary probability, and most likely secondary probability value, respectively for the Nocturnal-crepuscular hypothesis.
 #' @param xi.CR Default c(0.80,0.90). A vector of the lower threshold value and most likely value,, respectively for the Crepuscular hypothesis.
 #' @param xi.CRd  Default c(0.80,0.2,0.90,0.10). A vector of the lower threshold value for the primary probability, upper threshold value of the secondary probability, most likely primary probability, and most likely secondary probability value, respectively for the Nocturnal-crepuscular hypothesis.
@@ -32,11 +27,10 @@
 #' 
 #' @return diel.hyp A list of diel hypotheses as multinomial inequalities.
 #' \item{inputs}{Includes all inputted values; epsilon, xi, and p.avail.} 
-#' \item{D.th, D.max, D.var, Dn.th, Dn.max, Dn.var, Dc.th, Dc.max, Dc.var, Dcr.th,
-#' Dcr.max,Dcr.var, N.th, N.max, N.var, Nd.th, Nd.max, Nd.var, Nc.th,
-#' Nc.max, Nc.var, Ncr.th, Ncr.max, Ncr.var, CR.th, CR.max, CR.var,CRd.th,
-#' CRd.max, CRd.var, CRn.th, CRn.max, CRn.var, CRc.th, CRc.max, CRc.var,
-#'EC.th, EC.var, AC.var, C.th}{Each is a list of three elements: Hypotheis Descriptive Name, A matrix, and b vector.} 
+#' \item{D.th, D.max, D.var, Dn.th, Dn.max, Dn.var, Dc.max, Dcr.th,
+#' Dcr.max,Dcr.var, N.th, N.max, N.var, Nd.th, Nd.max, Nd.var, 
+#'  Nc.var, Ncr.th, Ncr.max, Ncr.var, CR.th, CR.max, CR.var,CRd.th,
+#' CRd.max, CRd.var, CRn.th, CRn.max, CRn.var,'EC.th, EC.var, AC.var, C.th}{Each is a list of three elements: Hypotheis Descriptive Name, A matrix, and b vector.} 
 #' @examples 
 #' diel.ineq()
 #' diel.ineq(e=0.01) #To replace all epsilon values with 0.01.
@@ -57,9 +51,9 @@
 
 # e  -  lower and upper difference to calculate range of probability values.
 
-# e varies by hypothesis, so:      e.D, e.Dn, e.Dc, e.Dcr, 
-#                                  e.N, e.Nd, e.Nc, e.Ncr, 
-#                                  e.CR, e.CRd, e.CRn, e.CRc,
+# e varies by hypothesis, so:      e.D, e.Dn, e.Dcr, 
+#                                  e.N, e.Nd, e.Ncr, 
+#                                  e.CR, e.CRd, e.CRn, 
 #                                  e.EC, e.AC
 
 # p.avail - (Available Cathemeral; length 2)
@@ -76,12 +70,6 @@
 #        [3] most likely prob. of Primary prob., 
 #        [4] most likely prob. of Secondary prob.
 
-# xi.Dc - (Diurnal-cathemeral Hyp.; length 4) 
-#        [1] lower prob. of Primary prob., 
-#        [2] lower prob. of Secondary prob., which should be above 0 to be cathemeral (e.g., 0.01).
-#        [3] most likely prob. of Primary prob., 
-#        [4] most likely prob. of Secondary prob (nightime and crepuscular probs).
-
 # xi.Dcr - (Diurnal-crepuscular; length 4)
 #          [1] lower prob. of Primary prob.  
 #          [2] upper prob. of Secondary prob (i.e., non-Diurnal periods). 
@@ -97,12 +85,6 @@
 #        [2] upper prob. of Secondary prob.,
 #        [3] most likely prob. of Primary prob., 
 #        [4] most likely prob. of Secondary prob.
-
-# xi.Nc - (Nocturnal-cathemeral Hyp.; length 4) 
-#        [1] lower prob. of Primary prob., 
-#        [2] lower prob. of Secondary prob., which should be above 0 to be cathemeral (e.g., 0.01).
-#        [3] most likely prob. of Primary prob., 
-#        [4] most likely prob. of Secondary prob (daytime and crepuscular probs.).
 
 # xi.Ncr - (Nocturnal-crepuscular; length 4)
 #          [1] lower prob. of Primary prob.  
@@ -126,11 +108,6 @@
 #          [3] most likely prob. of Primary prob., 
 #          [4] most likely prob. of Secondary prob.
 
-# xi.CRc - (Crepuscular-cathemeral; length 4)
-#        [1] lower prob. of Primary prob., 
-#        [2] lower prob. of Secondary prob., which should be above 0 to be cathemeral (e.g., 0.01).
-#        [3] most likely prob. of Primary prob., 
-#        [4] most likely prob. of Secondary prob (daytime and nocturnal probs.).
 
 # xi.EC - (Even Cathemeral; length 1)
 #        [1] most likely probability, which is applied to all
@@ -138,50 +115,44 @@
 
 #start function
 diel.ineq2=function(e=NULL,
-                   e.D=NULL, e.Dn=NULL, e.Dc=NULL, e.Dcr=NULL, 
-                   e.N=NULL, e.Nd=NULL, e.Nc=NULL, e.Ncr=NULL, 
-                   e.CR=NULL, e.CRd=NULL, e.CRn=NULL, e.CRc=NULL,
+                   e.D=NULL, e.Dn=NULL, e.Dcr=NULL, 
+                   e.N=NULL, e.Nd=NULL, e.Ncr=NULL, 
+                   e.CR=NULL, e.CRd=NULL, e.CRn=NULL,
                    e.EC=NULL, e.AC=NULL,
-                   xi.D=NULL, xi.Dn=NULL, xi.Dc=NULL,xi.Dcr=NULL,
-                   xi.N=NULL,xi.Nd=NULL,xi.Nc=NULL,xi.Ncr=NULL,
-                   xi.CR=NULL,xi.CRd=NULL,xi.CRn=NULL,xi.CRc=NULL,
+                   xi.D=NULL, xi.Dn=NULL, xi.Dcr=NULL,
+                   xi.N=NULL,xi.Nd=NULL,xi.Ncr=NULL,
+                   xi.CR=NULL,xi.CRd=NULL,xi.CRn=NULL,
                    xi.EC=NULL, p.avail=NULL){
   
   #Default epsilon values for variation hypotheses
   if(is.null(e.D)){e.D=0.05}
   if(is.null(e.Dn)){e.Dn=0.05}
-  if(is.null(e.Dc)){e.Dc=0.05}
   if(is.null(e.Dcr)){e.Dcr=0.05}
   if(is.null(e.N)){e.N=0.05}
   if(is.null(e.Nd)){e.Nd=0.05}
-  if(is.null(e.Nc)){e.Nc=0.05}
   if(is.null(e.Ncr)){e.Ncr=0.05}
   if(is.null(e.CR)){e.CR=0.05}
   if(is.null(e.CRd)){e.CRd=0.05}
   if(is.null(e.CRn)){e.CRn=0.05}
-  if(is.null(e.CRc)){e.CRc=0.05}
   if(is.null(e.EC)){e.EC=0.05}
   if(is.null(e.AC)){e.AC=0.1}
   
   # If e is entered then this forces all e's to be the same.  
-  if(!is.null(e)){e.D=e.Dn=e.Dc=e.Dcr=e.N=e.Nd=e.Nc=e.Ncr=e.CR=e.CRd=e.CRn=e.CRc=e.EC=e.AC=e}
+  if(!is.null(e)){e.D=e.Dn=e.Dcr=e.N=e.Nd=e.Ncr=e.CR=e.CRd=e.CRn=e.EC=e.AC=e}
   
   # Defaults for threshold and variation models     
   if(is.null(xi.D)){xi.D=c(0.90,0.95)} #lower threshold value, and most likely value
   if(is.null(xi.Dn)){xi.Dn=c(0.8,0.2,0.9,0.1)}
-  if(is.null(xi.Dc)){xi.Dc=c(0.8,0.01,0.9,0.1)}
   if(is.null(xi.Dcr)){xi.Dcr=c(0.8,0.2,0.9,0.1)}
   
   if(is.null(xi.N)){xi.N=c(0.8,0.9)}
   if(is.null(xi.Nd)){xi.Nd=c(0.8,0.2,0.9,0.1)}
-  if(is.null(xi.Nc)){xi.Nc=c(0.8,0.01,0.9,0.1)}
   if(is.null(xi.Ncr)){xi.Ncr=c(0.8,0.2,0.9,0.1)}
   
   if(is.null(xi.CR)){xi.CR=c(0.8,0.9)}
   if(is.null(xi.CRd)){xi.CRd=c(0.8,0.2,0.9,0.1)}
   if(is.null(xi.CRn)){xi.CRn=c(0.8,0.2,0.9,0.1)}
-  if(is.null(xi.CRc)){xi.CRc=c(0.8,0.01,0.9,0.1)}
-  
+
   if(is.null(xi.EC)){xi.EC  = c(0.33)}
   
   if(is.null(p.avail)){p.avail  = c(0.166666,0.4166667)}
@@ -220,25 +191,25 @@ diel.ineq2=function(e=NULL,
                 xi.Dn[4]+e.Dn-1,0.001)
   Dn.var=list("Diurnal-nocturnal Var",A.Dn.var,b.Dn.var)     
   
-  #################################
-  #Diurnal-cathemeral Hypotheses
-  # Threshold
-  A.Dc.th <- matrix(c(1,-1,-1,-2,-1,0,1,1,0,-1),ncol = 2, byrow = TRUE)
-  b.Dc.th <- c(0,-1,xi.Dc[2],1+xi.Dc[2],-xi.Dc[1])
-  Dc.th=list("Diurnal-cathemeral Threshold",A.Dc.th,b.Dc.th)     
-  
-  # Maximizing
-  A.Dc.max <- matrix(c(1,-1,-1,-2, -1, 0,1,1),ncol = 2, byrow = TRUE)
-  b.Dc.max <- c(0,-1,-xi.Dc[2],xi.Dc[2]+1)
-  Dc.max=list("Diurnal-cathemeral Max",A.Dc.max,b.Dc.max)     
-  
-  # Variability
-  A.Dc.var <- matrix(c(1,-1,-1,-2,-1,0,1,1,0,1,0,-1,1,0,-1,0,-1,-1,1,1),ncol = 2, byrow = TRUE)
-  b.Dc.var <- c(0,-1, -xi.Dc[2],xi.Dc[2]+1, xi.Dc[3]+e.Dc,
-                -xi.Dc[3]+e.Dc,xi.Dc[4]+e.Dc,-xi.Dc[4]+e.Dc,xi.Dc[4]+e.Dc-1,
-                -xi.Dc[4]+e.Dc+1)
-  Dc.var=list("Diurnal-cathemeral Var",A.Dc.var,b.Dc.var)     
-  
+  # #################################
+  # #Diurnal-cathemeral Hypotheses
+  # # Threshold
+  # A.Dc.th <- matrix(c(1,-1,-1,-2,-1,0,1,1,0,-1),ncol = 2, byrow = TRUE)
+  # b.Dc.th <- c(0,-1,xi.Dc[2],1+xi.Dc[2],-xi.Dc[1])
+  # Dc.th=list("Diurnal-cathemeral Threshold",A.Dc.th,b.Dc.th)     
+  # 
+  # # Maximizing
+  # A.Dc.max <- matrix(c(1,-1,-1,-2, -1, 0,1,1),ncol = 2, byrow = TRUE)
+  # b.Dc.max <- c(0,-1,-xi.Dc[2],xi.Dc[2]+1)
+  # Dc.max=list("Diurnal-cathemeral Max",A.Dc.max,b.Dc.max)     
+  # 
+  # # Variability
+  # A.Dc.var <- matrix(c(1,-1,-1,-2,-1,0,1,1,0,1,0,-1,1,0,-1,0,-1,-1,1,1),ncol = 2, byrow = TRUE)
+  # b.Dc.var <- c(0,-1, -xi.Dc[2],xi.Dc[2]+1, xi.Dc[3]+e.Dc,
+  #               -xi.Dc[3]+e.Dc,xi.Dc[4]+e.Dc,-xi.Dc[4]+e.Dc,xi.Dc[4]+e.Dc-1,
+  #               -xi.Dc[4]+e.Dc+1)
+  # Dc.var=list("Diurnal-cathemeral Var",A.Dc.var,b.Dc.var)     
+  # 
   #################################
   #Diurnal-crepuscular Hypotheses
   # Threshold
@@ -278,37 +249,37 @@ diel.ineq2=function(e=NULL,
   #Nocturnal-diurnal Hypotheses
   # Threshold
   A.Nd.th <- matrix(c(1,2,1,-1,1,1,0,1,1,0),ncol = 2, byrow = TRUE)
-  b.Nd.th <- c(1,0,-xi.Nd[1]+1,xi.Nd[2],0.01)
+  b.Nd.th <- c(1,0,-xi.Nd[1]+1,xi.Nd[2],0.001)
   Nd.th=list("Nocturnal-diurnal Threshold",A.Nd.th,b.Nd.th)     
   
   # Maximizing
   A.Nd.max <- matrix(c(1,2,1,-1,1,0),ncol = 2, byrow = TRUE)
-  b.Nd.max <- c(1,0,0.01)
+  b.Nd.max <- c(1,0,0.001)
   Nd.max=list("Nocturnal-diurnal Max",A.Nd.max,b.Nd.max)     
   
   # Variability
   A.Nd.var <- matrix(c(1,2,1,-1,-1,-1,1,1,0,1,0,-1,1,0),ncol = 2, byrow = TRUE)
-  b.Nd.var <- c(1,0,xi.Nd[3]+e.Nd-1,-xi.Nd[3]+e.Nd+1,xi.Nd[3]+e.Nd,-xi.Nd[4]+e.Nd,0.01)
+  b.Nd.var <- c(1,0,xi.Nd[3]+e.Nd-1,-xi.Nd[3]+e.Nd+1,xi.Nd[3]+e.Nd,-xi.Nd[4]+e.Nd,0.001)
   Nd.var=list("Nocturnal-diurnal Var",A.Nd.var,b.Nd.var)     
   
   #################################
-  #Nocturnal-cathemeral Hypotheses
-  # Threshold
-  A.Nc.th <- matrix(c(1,2,2,1,0,-1,-1,0,1,1),ncol = 2, byrow = TRUE)
-  b.Nc.th <- c(1,1,-xi.Nc[2],-xi.Nc[2],-xi.Nc[1]+1)
-  Nc.th=list("Nocturnal-cathemeral Threshold",A.Nc.th,b.Nc.th)     
-  
-  # Maximizing
-  A.Nc.max <- matrix(c(1,2,2,1,0,-1,-1,0),ncol = 2, byrow = TRUE)
-  b.Nc.max <- c(1,1,-xi.Nc[2],-xi.Nc[2])
-  Nc.max=list("Nocturnal-cathemeral Max",A.Nc.max,b.Nc.max)     
-  
-  # Variability
-  A.Nc.var <- matrix(c(1,2,2,1,0,-1,-1,0,1,1,-1,-1,-1,0,1,0),ncol = 2, byrow = TRUE)
-  b.Nc.var <- c(1,1,-xi.Nc[2],-xi.Nc[2],-xi.Nc[3]+e.Nc+1,xi.Nc[3]+e.Nc-1,
-                -xi.Nc[4]+e.Nc,xi.Nc[4]+e.Nc)
-  Nc.var=list("Nocturnal-cathemeral Var",A.Nc.var,b.Nc.var)     
-  
+  # #Nocturnal-cathemeral Hypotheses
+  # # Threshold
+  # A.Nc.th <- matrix(c(1,2,2,1,0,-1,-1,0,1,1),ncol = 2, byrow = TRUE)
+  # b.Nc.th <- c(1,1,-xi.Nc[2],-xi.Nc[2],-xi.Nc[1]+1)
+  # Nc.th=list("Nocturnal-cathemeral Threshold",A.Nc.th,b.Nc.th)     
+  # 
+  # # Maximizing
+  # A.Nc.max <- matrix(c(1,2,2,1,0,-1,-1,0),ncol = 2, byrow = TRUE)
+  # b.Nc.max <- c(1,1,-xi.Nc[2],-xi.Nc[2])
+  # Nc.max=list("Nocturnal-cathemeral Max",A.Nc.max,b.Nc.max)     
+  # 
+  # # Variability
+  # A.Nc.var <- matrix(c(1,2,2,1,0,-1,-1,0,1,1,-1,-1,-1,0,1,0),ncol = 2, byrow = TRUE)
+  # b.Nc.var <- c(1,1,-xi.Nc[2],-xi.Nc[2],-xi.Nc[3]+e.Nc+1,xi.Nc[3]+e.Nc-1,
+  #               -xi.Nc[4]+e.Nc,xi.Nc[4]+e.Nc)
+  # Nc.var=list("Nocturnal-cathemeral Var",A.Nc.var,b.Nc.var)     
+  # 
   #################################
   #Nocturnal-crepuscular Hypotheses
   # Threshold
@@ -350,17 +321,17 @@ diel.ineq2=function(e=NULL,
   #Crepuscular-diurnal Hypotheses
   # Threshold
   A.CRd.th <- matrix(c(-1,-2,-1,1,-1,0,0,1,-1,-1),ncol = 2, byrow = TRUE)
-  b.CRd.th <- c(-1,0,-xi.CRd[1],xi.CRd[2],-0.99)
+  b.CRd.th <- c(-1,0,-xi.CRd[1],xi.CRd[2],-0.9999)
   CRd.th=list("Crepuscular-diurnal Threshold",A.CRd.th,b.CRd.th)     
   
   # Maximizing
   A.CRd.max <- matrix(c(-1,-2,-1,1,-1,-1),ncol = 2, byrow = TRUE)
-  b.CRd.max <- c(-1,0,-0.99)
+  b.CRd.max <- c(-1,0,-0.9999)
   CRd.max=list("Crepuscular-diurnal Max",A.CRd.max,b.CRd.max)     
   
   # Variability
   A.CRd.var <- matrix(c(-1,-2,-1,1,-1,0,1,0,0,-1,0,1,-1,-1),ncol = 2, byrow = TRUE)
-  b.CRd.var <- c(-1, 0,-xi.CRd[3]+e.CRd,xi.CRd[3]+e.CRd,-xi.CRd[4]+e.CRd,xi.CRd[4]+e.CRd,-0.99)
+  b.CRd.var <- c(-1, 0,-xi.CRd[3]+e.CRd,xi.CRd[3]+e.CRd,-xi.CRd[4]+e.CRd,xi.CRd[4]+e.CRd,-0.9999)
   CRd.var=list("Crepuscular-diurnal Var",A.CRd.var,b.CRd.var)     
   
   #################################
@@ -381,24 +352,24 @@ diel.ineq2=function(e=NULL,
   CRn.var=list("Crepuscular-nocturnal Var",A.CRn.var,b.CRn.var)     
   
   #################################
-  #Crepuscular-cathemeral Hypotheses
-  # Threshold
-  A.CRc.th <- matrix(c(-1,1,-2,-1,0,-1,1,1,-1,0),ncol = 2, byrow = TRUE)
-  b.CRc.th <- c(0,-1,-xi.CRc[2],-xi.CRc[2]+1,-xi.CRc[1])
-  CRc.th=list("Crepuscular-cathemeral Threshold",A.CRc.th,b.CRc.th)     
-  
-  # Maximizing
-  A.CRc.max <- matrix(c(-1,1,-2,-1,0,-1,1,1),ncol = 2, byrow = TRUE)
-  b.CRc.max <- c(0,-1,-xi.CRc[2],-xi.CRc[2]+1)
-  CRc.max=list("Crepuscular-cathemeral Max",A.CRc.max,b.CRc.max)     
-  
-  # Variability
-  A.CRc.var <- matrix(c(-1,1,-2,-1,0,-1,1,1,-1,0,1,0,1,1,-1,-1,0,-1,0,1),ncol = 2, byrow = TRUE)
-  b.CRc.var <- c(0,-1,-xi.CRc[2],-xi.CRc[2]+1,-xi.CRc[3]+e.CRc,
-                 xi.CRc[3]+e.CRc,-xi.CRc[4]+e.CRc+1,xi.CRc[4]+e.CRc-1,
-                 -xi.CRc[4]+e.CRc,xi.CRc[4]+e.CRc)
-  CRc.var=list("Crepuscular-cathemeral Var",A.CRc.var,b.CRc.var)     
-  
+  # #Crepuscular-cathemeral Hypotheses
+  # # Threshold
+  # A.CRc.th <- matrix(c(-1,1,-2,-1,0,-1,1,1,-1,0),ncol = 2, byrow = TRUE)
+  # b.CRc.th <- c(0,-1,-xi.CRc[2],-xi.CRc[2]+1,-xi.CRc[1])
+  # CRc.th=list("Crepuscular-cathemeral Threshold",A.CRc.th,b.CRc.th)     
+  # 
+  # # Maximizing
+  # A.CRc.max <- matrix(c(-1,1,-2,-1,0,-1,1,1),ncol = 2, byrow = TRUE)
+  # b.CRc.max <- c(0,-1,-xi.CRc[2],-xi.CRc[2]+1)
+  # CRc.max=list("Crepuscular-cathemeral Max",A.CRc.max,b.CRc.max)     
+  # 
+  # # Variability
+  # A.CRc.var <- matrix(c(-1,1,-2,-1,0,-1,1,1,-1,0,1,0,1,1,-1,-1,0,-1,0,1),ncol = 2, byrow = TRUE)
+  # b.CRc.var <- c(0,-1,-xi.CRc[2],-xi.CRc[2]+1,-xi.CRc[3]+e.CRc,
+  #                xi.CRc[3]+e.CRc,-xi.CRc[4]+e.CRc+1,xi.CRc[4]+e.CRc-1,
+  #                -xi.CRc[4]+e.CRc,xi.CRc[4]+e.CRc)
+  # CRc.var=list("Crepuscular-cathemeral Var",A.CRc.var,b.CRc.var)     
+  # 
   #################################
   #################################
   #Even Cathemeral Hypotheses
@@ -457,29 +428,26 @@ diel.ineq2=function(e=NULL,
 
   ##################################################
   #Package Inputs
-  inputs=list(e.D=e.D, e.Dn=e.Dn, e.Dc=e.Dc, e.Dcr=e.Dcr, 
-              e.N=e.N, e.Nd=e.Nd, e.Nc=e.Nc, e.Ncr=e.Ncr, 
-              e.CR=e.CR, e.CRd=e.CRd, e.CRn=e.CRn, e.CRc=e.CRc,
+  inputs=list(e.D=e.D, e.Dn=e.Dn, e.Dcr=e.Dcr, 
+              e.N=e.N, e.Nd=e.Nd, e.Ncr=e.Ncr, 
+              e.CR=e.CR, e.CRd=e.CRd, e.CRn=e.CRn,
               e.EC=e.EC, e.AC=e.AC,
-              xi.D=xi.D, xi.Dn=xi.Dn, xi.Dc=xi.Dc,xi.Dcr=xi.Dcr,
-              xi.N=xi.N,xi.Nd=xi.Nd,xi.Nc=xi.Nc,xi.Ncr=xi.Ncr,
-              xi.CR=xi.CR,xi.CRd=xi.CRd,xi.CRn=xi.CRn,xi.CRc=xi.CRc,
+              xi.D=xi.D, xi.Dn=xi.Dn, xi.Dcr=xi.Dcr,
+              xi.N=xi.N,xi.Nd=xi.Nd,xi.Ncr=xi.Ncr,
+              xi.CR=xi.CR,xi.CRd=xi.CRd,xi.CRn=xi.CRn,
               xi.EC=xi.EC, p.avail=p.avail)  
   
   #package outputs  
   diel.hyp=list(   
     D.th=D.th, D.max=D.max,D.var=D.var,
     Dn.th=Dn.th,Dn.max=Dn.max,Dn.var=Dn.var,
-    Dc.th=Dc.th, Dc.max=Dc.max,Dc.var=Dc.var,
     Dcr.th=Dcr.th,Dcr.max=Dcr.max,Dcr.var=Dcr.var,
     N.th =N.th,N.max=N.max,N.var=N.var,
     Nd.th=Nd.th, Nd.max=Nd.max,Nd.var=Nd.var,
-    Nc.th=Nc.th,Nc.max=Nc.max,Nc.var=Nc.var,
     Ncr.th=Ncr.th,Ncr.max=Ncr.max,Ncr.var=Ncr.var,
     CR.th=CR.th,CR.max=CR.max,CR.var=CR.var,
     CRd.th=CRd.th,CRd.max=CRd.max,CRd.var=CRd.var,
     CRn.th=CRn.th,CRn.max=CRn.max,CRn.var=CRn.var,
-    CRc.th=CRc.th,CRc.max=CRc.max,CRc.var=CRc.var,
     EC.th=EC.th,EC.var=EC.var,AC.var=AC.var, C.th=C.th,
     inputs=inputs
   )
