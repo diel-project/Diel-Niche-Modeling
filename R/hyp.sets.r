b1247959b0e73ec03f65ee7f06a2e29a5a20d019
bgerber123 <bgerber@uri.edu>
2022-06-10 12:39:46 -0400

    Update hyp.sets.r
    with more combinations

--- R/hyp.sets.r
+++ R/hyp.sets.r
@@ -17,23 +17,28 @@
 hyp.sets=function(hyp.in=NULL){
 
   #NEED TO INCLUDE C.th somewhere
-  hyp.set=vector("list",9)
-  hyp.set[[1]]=c("D.th","N.th","CR.th","C.th")     #General Hypotheses
-  hyp.set[[2]]=c("D.max","N.max","CR.max")          #General Hypotheses
-  hyp.set[[3]]=c("D.var","N.var","CR.var","EC.var") #General Hypotheses
-  hyp.set[[4]]=c("Dn.th","Dcr.th","Nd.th","Ncr.th","CRd.th","CRn.th")          #Primary-secondary Hypotheses
-  hyp.set[[5]]=c("Dn.max","Dcr.max","Nd.max","Ncr.max","CRd.max","CRn.max") #Primary-secondary Hypotheses
-  hyp.set[[6]]=c("Dn.var","Dcr.var","Nd.var","Ncr.var","CRd.var","CRn.var") #Primary-secondary Hypotheses
-  hyp.set[[7]]=c(hyp.set[[1]],hyp.set[[4]]) #General & Primary-secondary Hypotheses
-  hyp.set[[8]]=c(hyp.set[[2]],hyp.set[[5]]) #General & Primary-secondary Hypotheses
-  hyp.set[[9]]=c(hyp.set[[4]],hyp.set[[6]]) #General & Primary-secondary Hypotheses
-  hyp.set[[10]]=c(hyp.set[[1]],hyp.set[[2]],hyp.set[[3]],hyp.set[[4]],hyp.set[[5]],hyp.set[[6]]) #All hypotheses; used for testing
+  hyp.set=vector("list",13)
+  hyp.set[[1]]=c("D.th","N.th","CR.th","EC.th")     #General Hypotheses
+  hyp.set[[2]]=c("D.th","N.th","CR.th","C.th")     #General Hypotheses
+  hyp.set[[3]]=c("D.max","N.max","CR.max")          #General Hypotheses
+  hyp.set[[4]]=c("D.var","N.var","CR.var","EC.var") #General Hypotheses
+  hyp.set[[5]]=c("D.var","N.var","CR.var","AC.var") #General Hypotheses
+  hyp.set[[6]]=c("Dn.th","Dcr.th","Nd.th","Ncr.th","CRd.th","CRn.th")          #Primary-secondary Hypotheses
+  hyp.set[[7]]=c("Dn.max","Dcr.max","Nd.max","Ncr.max","CRd.max","CRn.max") #Primary-secondary Hypotheses
+  hyp.set[[8]]=c("Dn.var","Dcr.var","Nd.var","Ncr.var","CRd.var","CRn.var") #Primary-secondary Hypotheses
+  hyp.set[[9]]=c(hyp.set[[1]],hyp.set[[6]]) #General & Primary-secondary Hypotheses
+  hyp.set[[10]]=c(hyp.set[[3]],hyp.set[[7]]) #General & Primary-secondary Hypotheses
+  hyp.set[[11]]=c(hyp.set[[4]],hyp.set[[8]]) #General & Primary-secondary Hypotheses
+  hyp.set[[12]]=c(hyp.set[[2]],hyp.set[[3]],hyp.set[[5]],
+                  hyp.set[[6]],hyp.set[[7]],hyp.set[[8]]) #All hypotheses; used for testing
+  hyp.set[[13]]=c(hyp.set[[1]],hyp.set[[3]],hyp.set[[4]],
+                  hyp.set[[6]],hyp.set[[7]],hyp.set[[8]]) #All hypotheses; used for testing
 
 #name the list hyp.set
-  names(hyp.set)=c("hyp.th","hyp.max","hyp.var",
+  names(hyp.set)=c("hyp.th","hyp.th2","hyp.max","hyp.var","hyp.var2",
                     "hyp.Ps.th","hyp.Ps.max","hyp.Ps.var",
                     "hyp.th.Ps.th","hyp.max.Ps.max","hyp.var.Ps.var",
-                   "hyp.all")
+                   "hyp.all","hyp.all2")
 if(!is.null(hyp.in)){ 
 #match the model set called for and return it
   hyp.set[[match(hyp.in,names(hyp.set))]]
