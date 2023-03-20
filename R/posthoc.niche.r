031510bf8028c25345ad1a5ca9c0330a07ffd894
bgerber123 <bgerber@uri.edu>
2023-03-20 15:26:06 -0400

    Update posthoc.niche.r

--- R/posthoc.niche.r
+++ R/posthoc.niche.r
@@ -22,7 +22,7 @@ if(!is.matrix(y)){
 if(ncol(y)!=3){
   stop("y needs to have three columns \n")
 }
-tol = 0.001 
+tol = 0.01 
 if(!all(apply(y,1,sum) >= 1-tol)){
   stop("all rows need to sum to 1 \n")
 }  
